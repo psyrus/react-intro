3611801217a09cad68f7c4a247828f4a48fccf42
Trevor Chinn <trevor.chinn@jp.kpmg.com>
2022-08-12 09:55:02 +0900

    Got styling working

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,15 +1,20 @@
 import { Component } from "react";
-
+import "./card-list.styles.css"
+import "./card.styles.css"
 class CardList extends Component {
 
     render() {
         console.log("Render from cardlist")
         console.log(this.props)
         return (
-            <div>
+            <div className="card-list">
                 {
                     this.props.content.map((item) => (
-                        <h1 key={item.id}>{item.name}</h1>
+                        <div className="card-container" key={item.id}>
+                            <img alt={`monster ${item.name}`} src={`https://robohash.org/${item.id}?set=set2&size=180x180`}/>
+                            <h2>{item.name}</h2>
+                            <p>{item.email}</p>
+                        </div>
                     ))
                 }
             </div>
