f7e025191485c33a1bd8b31a0fcebba739fcb90f
Trevor Chinn <trevor.chinn@jp.kpmg.com>
2022-08-19 09:40:06 +0900

    Add navigation and sign in

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
+      <Route path='/' element={<Navigation />}>
+        <Route index={true} element={<Home></Home>}></Route>
+        <Route path='sign-in' element={<SignIn />}></Route>
+      </Route>
     </Routes>
   );
 };
---
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = require('./firebase.config.json');
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
