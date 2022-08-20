9199e68838030229bcebef01c0dcd3eb2f595382
Trevor Chinn <trevor.chinn@jp.kpmg.com>
2022-08-20 17:12:05 +0900

    Also add redirect sign in

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,30 @@
-import { signInWithGooglePopup, createUserDocumentfromAuth } from "../../utils/firebase/firebase.utils";
-
+import { auth, signInWithGooglePopup, createUserDocumentfromAuth, signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await getRedirectResult(auth);
+            if (response) {
+                const userDocReferece = await createUserDocumentfromAuth(response.user)
+            }
+            console.log(response);
+        }
+        fetchData();
+    }, [])
+
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
         const userDocReferece = await createUserDocumentfromAuth(response.user);
     }
+
     return (
         <div>
             <h1>Sign in Page</h1>
-            <button onClick={logGoogleUser}>Sign in with Google</button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     )
 };
--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -13,13 +13,16 @@ const firebaseConfig = require('./firebase.config.json');
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 export const db = getFirestore();
 
 export const createUserDocumentfromAuth = async (userAuth) => {
