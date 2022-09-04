51065209e1a7454a9a52fbfbb7033b5f72352eff
Trevor Chinn <trevor.chinn@jp.kpmg.com>
2022-09-04 14:35:16 +0900

    Implemented it the udemy course way

--- crwn-clothing/src/contexts/cart.context.jsx
+++ crwn-clothing/src/contexts/cart.context.jsx
@@ -1,13 +1,34 @@
 import { createContext, useState } from "react"
 
+const addCartItem = (cartItems, productToAdd) => {
+    const existingCartItem = cartItems.find((item) => item.id === productToAdd.id);
+
+    console.log(existingCartItem);
+
+    if (existingCartItem) {
+        return cartItems.map((item) => item.id === productToAdd.id ? { ...item, quantity: item.quantity + 1 } : item)
+    }
+
+    return [...cartItems, { ...productToAdd, quantity: 1 }]
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setCartOpenState: () => null,
+    cartItems: [],
+    addItemToCart: () => { }
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setCartOpenState] = useState(false);
-    const value = { isCartOpen, setCartOpenState }
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = { isCartOpen, setCartOpenState, cartItems, addItemToCart }
+
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
