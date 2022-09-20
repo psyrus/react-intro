fcd059dbde3d80925cbe82b6384cee625e1b28be
psyrus <revolution.x@gmail.com>
2022-09-21 08:18:11 +0900

    Implemented user reducer

--- crwn-clothing/src/contexts/user.context.jsx
+++ crwn-clothing/src/contexts/user.context.jsx
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import { createContext, useEffect, useState } from "react";
 import { onAuthStateChangedListener, createUserDocumentfromAuth } from "../utils/firebase/firebase.utils";
 // This is what will be globally accessible
@@ -6,8 +7,42 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    console.log("User Reducer Dispatched");
+    console.log(action);
+    const { type, payload } = action;
+
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER: 
+            return {
+                currentUser: payload
+            };
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    const { currentUser } = state;
+    console.log(currentUser)
+
+    const setCurrentUser = (user) => {
+        dispatch({
+            type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user
+        });
+    }
+
+    // const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener(async (user) => {
