a84cd2143b09ec05e0e62ca8babb8760f7e385db
psyrus <revolution.x@gmail.com>
2023-01-25 01:32:14 +0900

    Began typescript conversion

--- monsters-rolodex/src/App.tsx
+++ monsters-rolodex/src/App.tsx
@@ -1,10 +1,26 @@
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
-class App extends Component {
-  constructor() {
-    super();
+import { getData } from './utils/data.utils';
+
+type Monster = {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface IProps {
+}
+
+interface IState {
+  searchString: string;
+  monsters: Monster[]
+}
+
+class App extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
 
     this.state = {
       searchString: '',
@@ -13,32 +29,38 @@ class App extends Component {
 
     console.log('Constructor completed');
   }
+  
 
   //TODO: I don't understand why this is running twice! It logs two times to the console on each render
   componentDidMount() {
     console.log('App Component Mounted');
+
+
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users');
+      this.setState(
+        () => {
+          return { apiMonsters: users, monsters: users, searchString: this.state.searchString };
+        },
+        () => {
+          console.log(this.state);
+        }
+      );
+    }
+
+    fetchUsers();
     // If you need to make an api request that must complete before the component originally renders, use this
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => {
-        return response.json();
-      })
-      .then((users) => {
-        this.setState(
-          () => {
-            return { apiMonsters: users, monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        );
-      });
   }
 
-  onSearchChange = (event) => {
+  onSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log("SOMETHING")
     this.setState(() => {
       return {
         searchString: event.target.value.toLocaleLowerCase(),
       };
+    },
+    () => {
+      console.log(this.state);
     });
   };
 
